d8f85bd2d2fa31e56d4d1ba72d7c087fa82f11be
tyler-wadekamper <wadekampert@spu.edu>
2021-11-30 10:17:04 -0800

    Finish playOneMatch skeleton

--- script.js
+++ script.js
@@ -1,55 +1,70 @@
-function playOneGame() {
+function playOneMatch() {
     let matchIsNotOver = true;
     let userInput = null;
 
     //display welcome message
-    displayWelcomeMessage();
+    printWelcomeMessage();
 
-    //while match not over
     while(matchIsNotOver){
-        //get user input
-        userInput = getUserInput();
+        let gameResult = null;
+        let userComputerMatchScore = [0, 0];
 
-        //validate user answer
-        userInputLowered = userInput.toLowerCase();
-
-        //generate cpu answer using a random number generator
-        
-
-        //resolve game based on user, cpu answers
+        //get the result of a game
+        gameResult = playOneGame();
         
         //change the score of the match
+        userComputerMatchScore = changeMatchScore(gameResult);
         
         //decide if match is over
+        if (matchIsNotOver) {
+            continue;
+        }
+        else {
+            printGameEndMessage();
+        }
     }
 }
 
-function displayWelcomeMessage() {
+function playOneGame(){
+    let keepGoing = true;
+        while (keepGoing) {
+            //get user input
+            userInput = getUserInputWithMessage();
+
+            //validate user input, decide if need to get input again
+            if (isValidUserInput(userInput)) {
+                keepGoing = false;
+            }
+            else {
+                printRetryUserInputMessage();
+            }
+        }
+
+        //generate cpu answer using a random number generator
+        computerInput = generateComputerInput();
+
+        //resolve game based on user, cpu answers
+
+}
+
+
+function printWelcomeMessage() {
     console.log("Welcome to rock paper scissors. Your options are 'rock', 'paper', or 'scissors'. Type your answer below.")
 }
 
-function getUserInput() {
+function getUserInputWithMessage() {
     return prompt("Choose your weapon. Your options are 'rock', 'paper', or 'scissors'.")
 }
 
 function isValidUserInput() {
-
+    userInputLowered = userInput.toLowerCase();
 }
 
 function generateComputerInput() {
 
 }
 
-function determineMatchOutcome(userInput, computerInput) {
-    let userVictory = null;
-
-    if (userInput == computerInput) {
-        
-    }
-
-    elif (userInput == "rock") {
-
-    }
+function determineMatchOutcome() {
 
 }
 
@@ -58,5 +73,9 @@ function updateMatchScore() {
 }
 
 function matchIsOver() {
+
+}
+
+function printGameEndMessage() {
 
 }
